f0ee9622a6684d528a93779d6b40ee5383f0b2f0
ksudheer02 <sudheerk6996@gmail.com>
2025-09-26 14:34:32 +0530

    updated the script

--- teradata-table-s3-bucket-backup/teradata-database-connection-verify.py
+++ teradata-table-s3-bucket-backup/teradata-database-connection-verify.py
@@ -1,5 +1,7 @@
 import sys
 import logging
+import boto3
+import json
 from awsglue.utils import getResolvedOptions
 from awsglue.context import GlueContext
 from awsglue.job import Job
@@ -11,7 +13,7 @@ logger.setLevel(logging.INFO)
 
 # Get job parameters
 args = getResolvedOptions(sys.argv,
-    ['JOB_NAME', 'TERADATA_CONNECTION_NAME', 'TERADATA_TABLE'])
+    ['JOB_NAME', 'TERADATA_JDBC_URL', 'TERADATA_SECRET_NAME', 'TERADATA_TABLE'])
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -19,17 +21,27 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-connection_name = args['TERADATA_CONNECTION_NAME']
+jdbc_url = args['TERADATA_JDBC_URL']
+secret_name = args['TERADATA_SECRET_NAME']
 table_name = args['TERADATA_TABLE']
 
+# Retrieve credentials from AWS Secrets Manager
+session = boto3.session.Session()
+client = session.client(service_name='secretsmanager')
+get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+secret = json.loads(get_secret_value_response['SecretString'])
+user = secret['username']
+password = secret['password']
+
 try:
-    logger.info(f"Attempting to connect to Teradata table: {table_name} using connection: {connection_name}")
+    logger.info(f"Attempting to connect to Teradata table: {table_name} using JDBC URL: {jdbc_url}")
     datasource = glueContext.create_dynamic_frame.from_options(
-        connection_type="teradata",
+        connection_type="jdbc",
         connection_options={
-            "connectionName": connection_name,
-            "dbtable": table_name,
-            "numRows": 1  # Limit to 1 row for quick verification
+            "url": jdbc_url,
+            "user": user,
+            "password": password,
+            "dbtable": table_name
         }
     )
     df = datasource.toDF()
